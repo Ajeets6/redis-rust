3082d148a6e221f8bb06ea8f5f96685186c06f36
Ajeets6 <itachis6234@gmail.com>
2023-05-13 00:08:16 +0530

    small cahnges

--- spawn_redis_server.sh
+++ spawn_redis_server.sh
@@ -8,5 +8,5 @@
 exec cargo run \
     --quiet \
     --release \
-    --target-dir=/tmp/codecrafters-redis-target \
+    --target-dir=/tmp/redis-rust \
     --manifest-path $(dirname $0)/Cargo.toml "$@"
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-// Uncomment this block to pass the first stage
 use std::net::{TcpListener,TcpStream};
 use std::io::{Write,Read};
 
 fn handle_connection(mut _stream: TcpStream) {
-  //...
+  
   let mut buf = [0; 256];
   loop{
   let read=_stream.read(&mut buf).unwrap();
@@ -13,11 +12,8 @@ fn handle_connection(mut _stream: TcpStream) {
 }
 
 fn main(){
-    //You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Started");
 
-    //Uncomment this block to pass the first stage
-    
+    println!("Started");
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     //listener.set_ttl(100).expect("could not set TTL");
     
